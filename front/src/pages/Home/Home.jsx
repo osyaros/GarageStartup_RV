e7df5cabd81edb41ddf349db677aca7b003d91f0
Osokin Yaroslav <osyaros.2005@yandex.ru>
2024-03-03 07:17:27 +0300

    END

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -25,8 +25,21 @@ export const Home = () => {
             <div className='home__wrap__extrablocks__att'>
               <span>Самый выгодный и <b>необычный</b> туризм</span>
             </div>
-            <div className='home__wrap__extablocks__block'>
-
+            <div className='home__wrap__extrablocks__block'>
+              <h2>
+                Автодома – это просто
+              </h2>
+              <span>
+                Давайте это докажем
+              </span>
+              <button>
+                <span>
+                решить вопросики
+                </span>
+                  <svg width="19" height="10" viewBox="0 0 19 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M18.472 6.08L14.512 9.992L13.192 8.672L15.16 6.704L15.136 6.488H0.496V4.376H15.136L15.16 4.184L13.192 2.216L14.512 0.871999L18.472 4.832V6.08Z" fill="black"/>
+                  </svg>
+              </button>
             </div>
           </div>
       </div>
